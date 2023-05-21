8ea3756e39d9eaeab34b3bbd1b36b093f189fd58
chad hinds <chadrickj8@gmail.com>
2023-05-21 10:49:43 -0400

    send test message on initial get request

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('<h1>Hi world!</h1>')
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT)
