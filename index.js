742050c6a39c76561fd96b8cb20afaeb31b7bf96
chad hinds <chadrickj8@gmail.com>
2023-05-21 12:57:03 -0400

    Serve the list on members on a specific route

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+const members = require('./Members');
 
 // Sets static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/members', (req, res) => {
+    res.json(members)
+})
 
 const PORT = process.env.PORT || 5001;
 
