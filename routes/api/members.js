4a9b5b8c23ae1b42d9b58708628679becc7b5f19
chad hinds <chadrickj8@gmail.com>
2023-05-24 11:18:34 -0400

    newMember

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const uuid = require('uuid')
 const router = express.Router();
-const members = require('../../Members')
+const members = require('../../Members');
 
 // Get all members
 router.get('/', (req, res) => {
-    res.json(members)
+    res.json(members);
 })
 
 // Get specific member
 router.get('/:id', (req, res) => {
-    const userID = req.params.id
+    const userID = req.params.id;
 
     const found = members.some(member => member.id === Number(userID))
 
@@ -18,6 +19,21 @@ router.get('/:id', (req, res) => {
     } else {
         res.status(400).json({ msg: `Member with id of ${userID} cannot be found` });
     }
+})
+
+// Create Member
+router.post('/', (req, res) => {
+    const newMember = {
+        ...req.body,
+        id: uuid.v4(),
+        status: 'active'
+    };
+    if (!newMember.name || !newMember.email) {
+        return res.status(400).json({ msg: 'Please include a name and email' });
+    }
+
+    members.push(newMember);
+    res.json(members);
 })
 
 module.exports = router;
