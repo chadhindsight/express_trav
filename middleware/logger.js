9dad96b7fd1e1fe5d968c3443db93a7118651d75
chad hinds <chadrickj8@gmail.com>
2023-05-24 10:26:27 -0400

    Move logger middleware logic to it's own dedicated file

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,1 +1,8 @@
 const moment = require('moment');
+
+const loggers = (req, res, next) => {
+    console.log(
+        `${req.protocol}://${req.get('host')}${req.originalUrl
+        }: ${moment().format()}`
+    );
+};
