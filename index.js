e70c78ff142f7e3066e3ffe391069dfba0328beb
chad hinds <chadrickj8@gmail.com>
2023-05-24 10:45:47 -0400

    Import members

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Members');
 const { logger } = require('./middleware/logger');
 
 const app = express();
